748c47fe4263ca92563309d0154eff2678c555ce
reuben <reuben@box.(none)>
2013-11-17 22:00:45 -0700

    committing polygon tester code and a modified version of the xml parser that does not use the xpath

---
+++ src/main/java/edu/boisestate/cs597/PolygonFrame.java
@@ -0,0 +1,63 @@
+package edu.boisestate.cs597;
+
+import java.awt.BasicStroke;
+import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import static java.awt.RenderingHints.KEY_ANTIALIASING;
+import static java.awt.RenderingHints.KEY_STROKE_CONTROL;
+import static java.awt.RenderingHints.VALUE_ANTIALIAS_ON;
+import static java.awt.RenderingHints.VALUE_STROKE_PURE;
+import java.awt.geom.Area;
+import java.awt.geom.Path2D;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class PolygonFrame {
+
+    private static JFrame mainMap;
+    private static Path2D.Double poly;
+
+    public PolygonFrame(int communityArea, final Path2D.Double poly)
+    {
+        this.poly = poly;
+        mainMap = new JFrame();
+        mainMap.setName("Community Area " + communityArea);
+        mainMap.setTitle("Community Area " + communityArea);
+        mainMap.setSize(500, 500);
+        mainMap.setContentPane(new Container() {
+            @Override
+            public void paint(Graphics graphics)
+            {
+                Graphics2D g2 = (Graphics2D) graphics;
+                g2.setStroke(new BasicStroke(5));
+                g2.setRenderingHint(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
+                g2.setRenderingHint(KEY_STROKE_CONTROL, VALUE_STROKE_PURE);
+                g2.draw(poly);
+            }
+        });
+        mainMap.setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+        Path2D.Double test = new Path2D.Double();
+
+        //random multiples to actually show something up
+        test.moveTo(87.524620*3, 41.691801*3);
+        test.lineTo(87.525012*5, 41.691805*2);
+        test.lineTo(87.525085*4, 41.691805*4);
+        test.lineTo(87.525085*3, 41.691805*5);
+        test.lineTo(87.525279*2, 41.691806*6);
+        test.closePath();
+        final Area area = new Area(test);
+        final Path2D.Double testTwin = new Path2D.Double(test);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run()
+            {
+                new PolygonFrame(52, testTwin);
+            }
+        });
+    }
+}
---
+++ src/main/java/edu/boisestate/cs597/PolygonTester.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.boisestate.cs597;
+
+import java.awt.geom.Path2D;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import javax.swing.SwingUtilities;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author reuben
+ */
+public class PolygonTester {
+
+    private static HashMap<Integer, Path2D.Double> communityPolygons = new HashMap<Integer, Path2D.Double>();
+    private static SimpleDateFormat sdf;
+
+    public static void setup(String kmlFile) throws IOException
+    {
+        sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss a");
+
+        if (!kmlFile.isEmpty())
+        {
+            Path pt = Paths.get(kmlFile);
+
+            try
+            {
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder;
+                dBuilder = dbFactory.newDocumentBuilder();
+                Document document = dBuilder.parse(pt.toFile());
+
+                document.getDocumentElement().normalize();
+
+                NodeList nodeList = document.getElementsByTagName("Placemark");
+                Path2D.Double poly;
+
+                for (int i = 0; i < nodeList.getLength(); i++)
+                {
+                    Node node = nodeList.item(i);
+
+                    if (node.getNodeType() == Node.ELEMENT_NODE)
+                    {
+                        Element eElement = (Element) node;
+                        String areaNumber = eElement.getElementsByTagName("name").item(0).getTextContent();
+                        String coordinates = eElement.getElementsByTagName("coordinates").item(0).getTextContent();
+                        System.out.println("community area: " + areaNumber);
+                        System.out.println("First Name : " + coordinates);
+                        
+                        poly = new Path2D.Double();
+
+                        String parts1[] = coordinates.split(" ");
+
+                        String parts2[];
+                        boolean moved = false;
+                        for (String coordinate : parts1)
+                        {
+                            if (!coordinate.isEmpty())
+                            {
+                                parts2 = coordinate.split(",");
+                                if (moved)
+                                {
+                                    poly.lineTo(Math.abs(new Double(parts2[0])), Math.abs(new Double(parts2[1])));
+                                }
+                                else
+                                {
+                                    poly.moveTo(Math.abs(new Double(parts2[0])), Math.abs(new Double(parts2[1])));
+                                    moved = true;
+                                }
+                            }
+                        }
+                        poly.closePath();
+
+                        try
+                        {
+                            communityPolygons.put(Integer.valueOf(areaNumber), poly);
+                        }
+                        catch (Exception e)
+                        {
+                            //we lose a large section of coordinates here
+                            continue;
+                        }
+
+                    }
+
+                }
+
+            }
+            catch (ParserConfigurationException | SAXException e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        setup(args[0]);
+        for (final Entry<Integer, Path2D.Double> entry : communityPolygons.entrySet())
+        {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run()
+                {
+                    new PolygonFrame(entry.getKey(), entry.getValue());
+                }
+            });
+
+        }
+    }
+}
