64e5ec860825ecbaff53eb0faef086cccb5a74dc
Gabriel Trisca <tog000@gmail.com>
2013-12-10 18:01:53 -0700

    Add final report

--- reports/generate_tree2.m
+++ reports/generate_tree2.m
@@ -1,11 +1,13 @@
 %% Import top 50.
 filename = 'top50.csv';
-delimiter = {','};
-formatSpec = '%*s%*s%s%*s%*s%*s%*s%*s%*s%*s%*s%*s%*s%*s%*s%[^\n\r]';
+delimiter = {'\t',','};
+formatSpec = '%*s%*s%s%*s%*s%*s%*s%*s%*s%*s%*s%*s%f%*s%*s%[^\n\r]';
 fileID = fopen(filename,'r');
 dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter,  'ReturnOnError', false);
+%dataArray2 = textscan(fileID, '%*s%*s%f%[^\n\r]', 'Delimiter', '\t',  'ReturnOnError', false);
 fclose(fileID);
 CRIME_NAMES = dataArray{:, 1};
+CRIME_COUNT = dataArray{:, 2};
 clearvars filename delimiter formatSpec fileID dataArray ans;
 
 %% Import most correlated.
@@ -42,8 +44,11 @@ indicators = keys(indicators);
 all_examples = zeros(1,length(indicators));
 all_classes = cell(1,1);
 
-crime_types={'BATTERY','SEX','ROBBERY'};
-crime_types_explained={'BATTERY','SEXUAL ASSAULT','ROBBERY'};
+%crime_types={'BATTERY','SEX','ROBBERY','HOMICIDE','RECKLESS'};
+%crime_types_explained={'BATTERY','SEXUAL ASSAULT','ROBBERY','HOMICIDE','RECKLESS CONDUCT'};
+crime_types={'BATTERY','SEX','ROBBERY','HOMICIDE'};
+crime_types_explained={'BATTERY','SEXUAL ASSAULT','ROBBERY','HOMICIDE'};
+taken_crimes = [];
 
 for ct=1:length(crime_types)
     
@@ -60,10 +65,20 @@ for ct=1:length(crime_types)
     end
     
     crime_indices = find(crime_type_indices(:,1)==1);
+    
+    taken_crimes = [taken_crimes crime_indices'];
+    
+    figure(1);
+    
     [h w] = size(all_examples);
     start_index = h;
     for i=1:length(crime_indices)
         for ind=1:length(indicators)
+            
+            if i == 1
+                subplot(round(length(indicators)/4),4,ind);
+            end
+            
             file_list = dir(sprintf('scatter/*%s*C%d-*',indicators{ind},crime_indices(i)));
             if length(file_list) >= 1
                 filename = ['scatter/' file_list(1).name];
@@ -73,11 +88,15 @@ for ct=1:length(crime_types)
                
                 [h w] = size(data);
                 if h > 20
+                    if i==1
+                        hold on;
+                        plot([mean(sorted(1:4,3)) mean(sorted(1:4,3))],[0 10],'r','LineWidth',2)
+                    end
                     all_examples(start_index+i,ind) = mean(sorted(1:4,3));
+                    all_classes{start_index+i} = crime_types_explained{ct};
                 end
             end
         end
-        all_classes{start_index+i} = crime_types_explained{ct};
     end
 end
 
@@ -89,14 +108,20 @@ all_examples = all_examples(sum2 ~= 0,:);
 
 %%
 
-plot(all_examples(find(all_examples(:,end)==1),2), 'ro');
+indices = 1:length(CRIME_NAMES);
+indices(taken_crimes) = [];
+CRIME_NAMES{indices}
+
+%%
+column = 2;
+plot(all_examples(find(all_classes=='BATTERY'),column), 'ro');
 hold on;
-plot(all_examples(find(all_examples(:,end)==2),2), 'go');
-plot(all_examples(find(all_examples(:,end)==3),2), 'bo');
+plot(all_examples(find(all_classes=='SEX'),column), 'bo');
+plot(all_examples(find(all_classes=='ROBBERY'),column), 'go');
 
 %%
 
-columns=[13 3 7 10];
+columns=[13 3 7];
 examples = all_examples(:,columns);
 classes = all_classes(2:end)';
 
@@ -107,6 +132,24 @@ classes = all_classes(2:end)';
 %classes = [1;1;1;1;2;2;2];
 
 %t=classregtree(examples,classes,'method','classification');
-tc = ClassificationTree.fit(examples,classes,'PredictorNames',indicators(columns),'minleaf',2);
+tc = ClassificationTree.fit(examples,classes,'PredictorNames',indicators(columns),'minleaf',3,'minparent',2);
 view(tc,'mode','graph') 
+
+%%
+
+figure(1);
+for ind=1:length(indicators)
+    
+    subplot(round(length(indicators)/4),4,ind);
+    
+    dirn=sprintf('scatter/*%sC1-*',indicators{ind});
+    file_list = dir(dirn);
+    if length(file_list) >= 1
+        filename = ['scatter/' file_list(1).name];
+        %disp(filename);
+        data=load(filename);
+        hist(data(:,3),10);
+        title(strrep(indicators{ind},'_',' '));
+    end
+end
 
